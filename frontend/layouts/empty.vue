55ce9ef6902f40e1c7d7a0530c18c12bae84a6e9
Nitesh Kumar Niranjan <niteshkumarniranjan@gmail.com>
2019-03-30 04:01:02 +0530

    Added Camera Access

---
+++ frontend/layouts/empty.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="font-sans min-h-screen antialiased text-grey relative">
+    <div class="body-content">
+      <nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {}
+}
+</script>
