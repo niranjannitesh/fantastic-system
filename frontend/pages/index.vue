a791f7151796480a1c63d807396cded79f3f3058
Nitesh Kumar Niranjan <niteshkumarniranjan@gmail.com>
2019-03-30 10:45:46 +0530

    Added Signup and HomePage

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,10 +1,20 @@
 <template>
-
+  <div>
+    <h4 class="text-center pt-6">Welcome!</h4>
+    <div class="p-4">
+      <button class="w-full bg-blue text-white rounded h-64 flex items-center justify-center mb-6">
+        <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="48" height="48"><path class="heroicon-ui" d="M6 2h9a1 1 0 0 1 .7.3l4 4a1 1 0 0 1 .3.7v13a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V4c0-1.1.9-2 2-2zm9 2.41V7h2.59L15 4.41zM18 9h-3a2 2 0 0 1-2-2V4H6v16h12V9zm-5 4h2a1 1 0 0 1 0 2h-2v2a1 1 0 0 1-2 0v-2H9a1 1 0 0 1 0-2h2v-2a1 1 0 0 1 2 0v2z"/></svg>
+      </button>
+      <button class="w-full bg-blue text-white rounded h-64 flex items-center justify-center">
+        <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="48" height="48"><path class="heroicon-ui" d="M6 14H4a2 2 0 0 1-2-2V4c0-1.1.9-2 2-2h12a2 2 0 0 1 2 2v2h2a2 2 0 0 1 2 2v13a1 1 0 0 1-1.7.7L16.58 18H8a2 2 0 0 1-2-2v-2zm0-2V8c0-1.1.9-2 2-2h8V4H4v8h2zm14-4H8v8h9a1 1 0 0 1 .7.3l2.3 2.29V8z"/></svg>
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  layout: 'tab'
 }
 </script>
 
